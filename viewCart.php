0bc734263e185808f5416fbd4e3762ead5965563
ptrnkOstap <ptrnkostap@gmail.com>
2021-06-20 21:02:42 +0300

    'HW7_fixes'

---
+++ viewCart.php
@@ -0,0 +1,33 @@
+<?php
+include 'functions.php';
+authUser();
+?>
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <title>Title</title>
+    <link href="public/css/style.css" type="text/css" rel="stylesheet">
+</head>
+<body>
+
+<div class="gallery_wrapper">
+    <div class="header">
+        <a href="index.php"><h1> Hello</h1></a>
+        <h1> Your cart</h1>
+        <div class="login_form"><?php authForm() ?></div>
+    </div>
+    <div class="products">
+        <?php
+        renderShoppingCart();
+
+        ?>
+    </div>
+
+
+</div>
+
+
+</body>
+</html>
+
